91671aeba1ddc3d1e4b3dc2f6799221d6d4a22ec
rabiayazici <rabiiayazici@gmail.com>
2024-12-27 16:10:55 +0300

    search screen db dışında tamamlandı

--- app/src/main/java/com/example/fizyoapp/ui/bottomnavbar/BottomNavbarComponent.kt
+++ app/src/main/java/com/example/fizyoapp/ui/bottomnavbar/BottomNavbarComponent.kt
@@ -1,22 +1,14 @@
 package com.example.fizyoapp.ui.bottomnavbar
-
-
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
-import com.example.fizyoapp.navigation.AppScreens
 
 @Composable
 fun BottomNavbarComponent(navController: NavController){
